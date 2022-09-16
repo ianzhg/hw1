0c62972dada3475fefd485c41276480b2b225fa6
ianzhg <ianzyx@hotmail.com>
2022-09-16 17:24:51 -0400

    initializewd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import { Component } from "react";
+import { Container, List, Fab} from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Add } from '@mui/icons-material';
+import Book from "./components/Book";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles({
+  fab: {
+    position: 'absolute',
+    bottom: "2rem",
+    right: "2rem",
+  }
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [
+        {
+          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut.",
+        },
+        {
+          title: "Cras bibendum libero eu viverra tristique.",
+        },
+        {
+          title: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.",
+        },
+      ],
+    };
+  }
+
+  render() {
+    const {books} = this.state;
+    return (
+      <Container>
+        <List>
+          {books.map((book, index) => {
+            return <Book book={book} key={index}/>
+          })}
+        </List>
+        <Fab aria-label={"Add"} className={classes.fab}>
+          <Add />
+        </Fab>
+      </Container>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
